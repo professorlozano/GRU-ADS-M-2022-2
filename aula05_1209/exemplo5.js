46039a49feed33875dd662dffa64c4ce1ea7c27b
Professor Lozano <luizmlozano@gmail.com>
2022-09-12 16:44:15 -0300

    aula 05

---
+++ aula05_1209/exemplo5.js
@@ -0,0 +1,13 @@
+let num1;
+let num2;
+let resultado;
+
+//num1 = 10;
+num1 = Number(prompt("Digite o valor do primeiro numero:"));
+//num2 = 20;
+num2 = Number(prompt("Digite o valor do segundo numero:"))
+
+
+resultado = num1 + num2;
+//console.log(resultado); //impressão no terminal
+alert(resultado);
